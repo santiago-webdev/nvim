636e25a2e696ddeef4bc148e063bacdad963863c
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2024-05-19 14:48:18 +0000

    feat: add settings directory as a valid configuration path

--- init.lua
+++ init.lua
@@ -1,7 +1,5 @@
 vim.loader.enable()
 
-require "user.settings"
-
 -- Function to import files that return plugin specs in table format for lazy.nvim to manage
 -- https://github.com/folke/lazy.nvim?tab=readme-ov-file#-plugin-spec
 require "user.spec"
@@ -15,3 +13,6 @@ spec "user.external.project"
 spec "user.external.telescope"
 
 require "user.lazy"
+
+local config_path = vim.fn.stdpath("config")
+package.path = package.path .. ";" .. config_path .. "/settings/init.lua;" .. config_path .. "/settings/?.lua" .. config_path .. "/settings/?/init.lua;"
