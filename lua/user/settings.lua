4e24a6a08036fb146071ab2a6db46e7c8b588765
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2024-05-20 01:12:47 +0000

    feat: dynamic gutter numbers

--- lua/user/settings.lua
+++ lua/user/settings.lua
@@ -1,4 +1,3 @@
-vim.opt.number = true -- Line numbers
 vim.opt.clipboard = "unnamedplus" -- Share the clipboard with your DE/OS
 vim.opt.confirm = true -- Instead of failing to exit because there's unsaved changes, it will ask for confirmation
 vim.opt.expandtab = true -- For insert mode, pressing TAB will insert spaces instead
@@ -17,3 +16,25 @@ vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "NONE" })
 -- Hide status and command line
 vim.opt.laststatus = 0
 vim.opt.cmdheight = 0
+
+-- Gutter numbers, dynamically change based on mode
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.api.nvim_create_autocmd({
+  "CmdlineEnter",
+  "InsertEnter",
+}, {
+  callback = function()
+    vim.opt.relativenumber = false
+  end,
+})
+
+vim.api.nvim_create_autocmd({
+  "CmdlineLeave",
+  "InsertLeave",
+}, {
+  callback = function()
+    vim.opt.relativenumber = true
+  end,
+})
