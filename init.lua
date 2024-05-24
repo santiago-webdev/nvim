3a539cd172ec1a5ab2de277ff0c9fece69c620b9
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2024-05-24 19:21:34 -0300

    feat: remove setting not needed anymore
    Because of Editorconfig

--- init.lua
+++ init.lua
@@ -3,7 +3,6 @@ vim.loader.enable()
 -- Settings {{{
 vim.opt.clipboard = "unnamedplus" -- Share the clipboard with your DE/OS
 vim.opt.confirm = true -- Instead of failing to exit because there's unsaved changes, it will ask for confirmation
-vim.opt.expandtab = true -- For insert mode, pressing TAB will insert spaces instead
 vim.opt.ignorecase = true -- Makes search feel more intuitive
 vim.opt.smartcase = true -- Ignorecase has to be true or this doesn't have any effect
 vim.opt.scrolloff = 9999 -- Keep the cursor centered
