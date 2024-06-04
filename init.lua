2f33c4e3181a1bef9a6e88160521edabde4ed440
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2024-06-04 20:05:06 -0300

    feat: more comments

--- init.lua
+++ init.lua
@@ -2,13 +2,14 @@ require "user"
 
 require "user.plugins.spec"
 spec "user.plugins.gitsigns"
-spec "user.plugins.lspconfig"
-spec "user.plugins.mason"
-spec "user.plugins.none-ls"
 spec "user.plugins.project"
 spec "user.plugins.telescope"
 spec "user.plugins.indentlines"
 spec "user.plugins.firenvim"
+-- LSP
+spec "user.plugins.lspconfig"
+spec "user.plugins.mason"
+spec "user.plugins.none-ls"
 -- Plugins that do it better than builtins
 spec "user.plugins.treesitter" -- Syntax
 spec "user.plugins.cmp" -- Completion
