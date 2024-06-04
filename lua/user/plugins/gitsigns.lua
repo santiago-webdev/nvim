9c7b252cc86108e9f3964fb067e770c6e5b95274
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2024-06-04 20:04:11 -0300

    feat: simplify gitsigns.nvim config

--- lua/user/plugins/gitsigns.lua
+++ lua/user/plugins/gitsigns.lua
@@ -6,52 +6,10 @@ local M = {
 
 M.config = function()
   require("gitsigns").setup {
-    signs = {
-      add = {
-        hl = "GitSignsAdd",
-        -- text = icons.ui.BoldLineMiddle,
-        numhl = "GitSignsAddNr",
-        linehl = "GitSignsAddLn",
-      },
-      change = {
-        hl = "GitSignsChange",
-        -- text = icons.ui.BoldLineDashedMiddle,
-        numhl = "GitSignsChangeNr",
-        linehl = "GitSignsChangeLn",
-      },
-      delete = {
-        hl = "GitSignsDelete",
-        -- text = icons.ui.TriangleShortArrowRight,
-        numhl = "GitSignsDeleteNr",
-        linehl = "GitSignsDeleteLn",
-      },
-      topdelete = {
-        hl = "GitSignsDelete",
-        -- text = icons.ui.TriangleShortArrowRight,
-        numhl = "GitSignsDeleteNr",
-        linehl = "GitSignsDeleteLn",
-      },
-      changedelete = {
-        hl = "GitSignsChange",
-        -- text = icons.ui.BoldLineMiddle,
-        numhl = "GitSignsChangeNr",
-        linehl = "GitSignsChangeLn",
-      },
-    },
-    watch_gitdir = {
-      interval = 1000,
-      follow_files = true,
-    },
-    attach_to_untracked = true,
-    current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
-    update_debounce = 200,
-    max_file_length = 40000,
+    signcolumn = false,
+    numhl = true,
     preview_config = {
       border = "rounded",
-      style = "minimal",
-      relative = "cursor",
-      row = 0,
-      col = 1,
     },
   }
 end
