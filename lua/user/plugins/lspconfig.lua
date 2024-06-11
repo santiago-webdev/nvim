fa233a95080a7232ba675662346d9849c14761a5
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2024-06-11 19:50:42 -0300

    feat: settings in root init.lua and use folds

--- lua/user/plugins/lspconfig.lua
+++ lua/user/plugins/lspconfig.lua
@@ -29,7 +29,8 @@ local function lsp_keymaps(bufnr)
   keymap(bufnr, "n", "gI", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
   keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
   keymap(bufnr, "n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
-  vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+  vim.keymap.set("n", "gf", vim.lsp.buf.definition)
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition) -- I'm unsure which one is better for this keymap
 end
 
 M.on_attach = function(client, bufnr)
