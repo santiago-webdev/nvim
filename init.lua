6b9ddf2c7b9abb35f4df5fb6c3d2feb92f57061a
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2024-07-28 10:41:06 -0300

    feat: remove plugins/init.lua and include them in init.lua

--- init.lua
+++ init.lua
@@ -142,4 +142,23 @@ vim.api.nvim_create_autocmd("FocusLost", {
   end,
 })
 
-require("user.plugins")
+require("user.plugins.spec")
+
+spec("user.plugins.gitsigns")
+spec("user.plugins.project")
+spec("user.plugins.telescope")
+spec("user.plugins.indentlines")
+spec("user.plugins.firenvim")
+spec("user.plugins.lspconfig")
+spec("user.plugins.mason")
+spec("user.plugins.none-ls")
+spec("user.plugins.nvim-jdtls")
+spec("user.plugins.accelerated-jk")
+
+-- Plugins that replace builtin tooling
+spec("user.plugins.treesitter")
+spec("user.plugins.cmp")
+spec("user.plugins.comment")
+spec("user.plugins.ufo")
+
+require("user.plugins.lazy")
