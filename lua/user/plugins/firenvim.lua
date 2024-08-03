91f98a6eb543701f911e30ab65893d3acd810c6e
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2024-08-03 10:25:14 -0300

    feat: update firenvim install method

--- lua/user/plugins/firenvim.lua
+++ lua/user/plugins/firenvim.lua
@@ -1,11 +1,6 @@
 local M = {
   "glacambre/firenvim",
-  -- Lazy load firenvim
-  -- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
-  lazy = not vim.g.started_by_firenvim,
-  build = function()
-    vim.fn["firenvim#install"](0)
-  end,
+  build = ":call firenvim#install(0)"
 }
 
 function M.config()
