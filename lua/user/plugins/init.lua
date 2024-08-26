e7a8c9f7c20cc1407f5e5584e396f64739b8930c
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2024-08-25 22:02:14 -0300

    feat: move plugins to plugins/init.lua

---
+++ lua/user/plugins/init.lua
@@ -0,0 +1,21 @@
+require("user.plugins.spec")
+
+spec("user.plugins.gitsigns")
+spec("user.plugins.project")
+spec("user.plugins.telescope")
+spec("user.plugins.indentlines")
+spec("user.plugins.firenvim")
+spec("user.plugins.lspconfig")
+spec("user.plugins.mason")
+spec("user.plugins.none-ls")
+spec("user.plugins.nvim-jdtls")
+spec("user.plugins.accelerated-jk")
+spec("user.plugins.nvim-colorizer")
+
+-- Plugins that replace builtin tooling
+spec("user.plugins.treesitter")
+spec("user.plugins.cmp")
+spec("user.plugins.comment")
+spec("user.plugins.ufo")
+
+require("user.plugins.lazy")
